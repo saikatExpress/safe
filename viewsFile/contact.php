34870ae1fe2410eba42d8dee87d1ec9030e66c68
Saikat724 <talukdersaikat444@gmail.com>
2023-01-25 02:46:34 +0600

    Some chnages

--- viewsFile/contact.php
+++ viewsFile/contact.php
@@ -2,7 +2,106 @@
 
 
 
-<h4>Contact Page</h4>
+<div class="row customRow">
+
+    <h2>For emergency contact</h2>
+
+    <div class="col-lg-4">
+        <div class="mycolumn">
+            <img src="images/masum.jpg" alt="no images">
+            <h4><i class="fa fa-user" aria-hidden="true"></i> Rofiqul Islam Masum</h4>
+            <mark>Teachnical Support</mark>
+            <p>
+                <i class="fa fa-envelope" aria-hidden="true"></i> masum@gmail.com
+            </p>
+            <p>
+                <i class="fa fa-phone" aria-hidden="true"></i> 01751-810216
+            </p>
+
+            <p>
+                <i class="fa fa-globe" aria-hidden="true"></i> Mirpur 10 , Dhaka
+            </p>
+        </div>
+    </div>
+
+    <div class="col-lg-4">
+        <div class="mycolumn">
+            <img src="images/sajjad.jpg" alt="no images">
+            <h4><i class="fa fa-user" aria-hidden="true"></i> Sajjad Hossain</h4>
+
+
+            <mark>Bussiness Support</mark>
+
+            <p>
+                <i class="fa fa-envelope" aria-hidden="true"></i> sajjad@gmail.com
+            </p>
+            <p>
+                <i class="fa fa-phone" aria-hidden="true"></i> 01751-810216
+            </p>
+            <p>
+                <i class="fa fa-globe" aria-hidden="true"></i> Square Hospital , Kolabagan , Dhaka
+            </p>
+        </div>
+    </div>
+
+    <div class="col-lg-4">
+        <div class="mycolumn">
+            <img src="images/juthe.jpg" alt="no images">
+            <h4><i class="fa fa-user" aria-hidden="true"></i> Juthe Croze</h4>
+            <mark>Design Support</mark>
+            <p>
+                <i class="fa fa-envelope" aria-hidden="true"></i> juthe@gmail.com
+            </p>
+            <p>
+                <i class="fa fa-phone" aria-hidden="true"></i> 01751-810216
+            </p>
+            <p>
+                <i class="fa fa-globe" aria-hidden="true"></i> Honda goli , Tejgaon , Dhaka.
+            </p>
+        </div>
+    </div>
+
+
+
+</div>
+
+
+
+
+<div class="row customRow">
+
+
+    <h4>For more Contact</h4>
+
+    <div class="col-lg-4">
+        <h4>Phone Number</h4>
+    </div>
+
+    <div class="col-lg-4">
+        <h4>Email</h4>
+    </div>
+
+    <div class="col-lg-4">
+        <h4>Address</h4>
+    </div>
+
+
+</div>
+
+
+<div class="row customRow">
+
+    <h4>Location in Map</h4>
+
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d58419.76044728939!2d90.37683846533135!3d23.774643654924645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1ssonargaon%20university!5e0!3m2!1sen!2sbd!4v1674591610697!5m2!1sen!2sbd"
+        width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+
+
+
+</div>
 
 
 
--- viewsFile/helpline.php
+++ viewsFile/helpline.php
@@ -6,7 +6,132 @@
 
 
 
-<h4>Help Line</h4>
+<div class="row customRow">
+
+    <div class="help_center_page">
+        <div class="help_info">
+            <h4>নির্দেশনাবলী</h4>
+
+            <ul>
+                <li>প্রথমে <b>Sign Up </b> লেখাটার উপর ক্লিক করুন ।</li>
+                <li>আপনার পুরো নাম সঠিকভাবে লিখবেন।</li>
+                <li>আপনার সচল ফোন নাম্বার দিয়ে রেজিস্ট্রেশন করুন।</li>
+                <li>সঠিক ইমেইল দিয়ে রেজিস্টেশন করুন।</li>
+                <li>আপনার ইমেইলে একটা OTP কোড যাবে,সেটা দিয়ে ইমেইল কনফার্ম করুন।</li>
+                <li>তারপর <b>Submit</b> বাটনে ক্লিক করলে , আপনাকে <b>Log in</b> পেইজে নিয়ে যাবে।</li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+
+<div class="row customRow">
+
+    <div class="complains_tutorial">
+        <h4>How to Complain ?</h4>
+        <p>
+            You have three categories to complain's.
+        </p>
+        <ul>
+            <li>Police</li>
+            <li>Fire Service</li>
+            <li>Ambulance</li>
+        </ul>
+    </div>
+
+    <div class="col-lg-4">
+        <div class="column4">
+            <div class="column4_title">
+                <h4>Police</h4>
+                <img src="https://cdn-icons-png.flaticon.com/128/3270/3270987.png" alt="no images">
+            </div>
+            <div class="column_para">
+                <p>
+                    If you encounter an unexpected incident in your daily life, such as a terrorist, harasser,
+                    suspicious person, for such incidents, you can directly enter our police page and file a complaint
+                    in the complaint box and if you do not know the location, then in which district or police station
+                    or in any area, you can write the address. .And if you don't know it at all, then it will work even
+                    if you don't give the location.
+                    thank you.
+                </p>
+            </div>
+
+            <div class="column_box">
+                <h4>Police Complain Form</h4>
+                <form action="" method="post">
+                    <label for="complain">Complain : </label> <br>
+                    <input type="text"> <br>
+                    <label for="location">Location : </label> <br>
+                    <input type="text"> <br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4">
+        <div class="column4">
+            <div class="column4_title">
+                <h4>Fire Service</h4>
+                <img src="https://cdn-icons-png.flaticon.com/128/7072/7072045.png" alt="no images">
+            </div>
+            <div class="column_para">
+                <p>
+                    Fire is one of the most dangerous things in people's life. Gas stove catches fire accidentally.
+                    Short circuit of current also causes fire. If the fire is serious, enter our fire service page and
+                    write the complaint in the complaint box and the name of the place (even if the problem is not
+                    written). No) submit. Try to keep your mobile number open. Your nearest Farai service team will come
+                    to your rescue.
+                    thank you
+                </p>
+            </div>
+
+            <div class="column_box">
+                <h4>Fire Service Complain Form</h4>
+                <form action="" method="post">
+                    <label for="complain">Complain : </label> <br>
+                    <input type="text"> <br>
+                    <label for="location">Location : </label> <br>
+                    <input type="text"> <br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col-lg-4">
+        <div class="column4">
+            <div class="column4_title">
+                <h4>Ambulance</h4>
+                <img src="https://cdn-icons-png.flaticon.com/128/2894/2894975.png" alt="no images">
+            </div>
+            <div class="column_para">
+                <p>
+                    Road accidents have increased in our country these days. Besides, it is difficult to tell when
+                    people are in danger. We have started the ambulance service for your service. You go somewhere and
+                    get sick or have an accident on the road. You are not getting any help anywhere, if you want. You
+                    can apply for help by visiting the ambulance page of our website.Your are most welcome to our
+                    website.
+                    Thank you
+                </p>
+            </div>
+
+            <div class="column_box">
+                <h4>Ambulance Complain Form</h4>
+                <form action="" method="post">
+                    <label for="complain">Complain : </label> <br>
+                    <input type="text"> <br>
+                    <label for="location">Location : </label> <br>
+                    <input type="text"> <br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+</div>
 
 
 
